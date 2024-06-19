27eaa871f816d06e6097c3057274b13b3c46aeb0
adam smith <adam00199382818@gmail.com>
2024-06-19 12:36:54 +0100

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,29 +1,42 @@
+import { clear } from "console";
 import * as net from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const acceptedPaths: string[] = ["/", "/index.html"];
+const acceptedPaths = ["/", "/index.html", "/echo/?.*"] as const;
 const r200 = "HTTP/1.1 200 OK\r\n\r\n"
 const r404 = "HTTP/1.1 404 Not Found\r\n\r\n"
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  const response = (res: string, body: string = ""): void => {
+    socket.write(res.concat(body));
+    socket.end();
+  };
   socket.on("data", (data) => {
     const httpRequest = new TextDecoder().decode(data);
-    const [request, ...headers] = httpRequest.split("\r\n");
-    const regex = /^GET (?<path>.*) HTTP\/1.1/.exec(request)?.groups;
-    const path = regex?.path;
-    if (!path) {
-      socket.write(r404);
+    const regex = /^(?<method>GET|POST|PUT|PATCH) (?<path>\S+) HTTP\/(?<version>\d\.\d)\r\n(?<headers>.*\r\n)\r\n$/gms.exec(httpRequest)?.groups;
+    if (!regex) {
+      response(r404);
+      return;
     }
+    const { method, path, version, headers } = regex;
     for (let i = 0; i < acceptedPaths.length; i++) {
-      const acceptedPath = acceptedPaths[i];
-      if (acceptedPath === path) {
-        socket.write(r200);
+      const acceptedPath: typeof acceptedPaths[number] = acceptedPaths[i];
+      const regexp = new RegExp(`^${acceptedPath}$`);
+      if (regexp.test(path)) {
+        if (acceptedPath.startsWith("/echo")) {
+          const endpoint = path.split("/")[2];
+          if (endpoint) {
+            response(r200, `Content-Type: text/plain\r\nContent-Length: ${endpoint.length}\r\n\r\n${endpoint}`);
+            return;
+          }
+        }
+        response(r200);
         return;
       }
     }
-    socket.write(r404);
+    response(r404);
   });
   socket.on("close", () => {
     socket.end();
