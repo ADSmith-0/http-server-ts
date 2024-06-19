9d7bcee22970501c5d86240d3c4243e4de28102f
adam smith <adam00199382818@gmail.com>
2024-06-19 22:42:45 +0100

    refactored

--- app/main.ts
+++ app/main.ts
@@ -3,43 +3,79 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const acceptedPaths = ["/", "/index.html", "/echo/?.*"] as const;
-const r200 = "HTTP/1.1 200 OK\r\n"
-const r404 = "HTTP/1.1 404 Not Found\r\n"
+type Response = (response: string, body?: string) => void;
+
+type Request = {
+	method: "GET" | "POST" | "PUT" | "PATCH";
+	path: string;
+	httpVersion: string;
+	headers: { [key: string]: string };
+	body: string | undefined;
+	response: Response;
+};
+
+const _200 = "HTTP/1.1 200 OK\r\n";
+const _404 = "HTTP/1.1 404 Not Found\r\n";
+
+const requestHandler: { [key: string]: (request: Request) => void } = {
+	"/": ({ response }) => {
+		response(_200);
+	},
+	"/index.html": ({ response }) => {
+		response(_200);
+	},
+	"/echo/?.*": ({ path, headers, response }) => {
+		console.log("headers:", headers);
+		const endpoint = path.split("/")[2];
+		if (endpoint) {
+			response(
+				_200,
+				`Content-Type: text/plain\r\nContent-Length: ${endpoint.length}\r\n\r\n${endpoint}`,
+			);
+			return;
+		}
+	},
+} as const;
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  const response = (res: string, body: string = ""): void => {
-    socket.write(res.concat(body, "\r\n"));
-    socket.end();
-  };
-  socket.on("data", (data) => {
-    const httpRequest = new TextDecoder().decode(data);
-    const regex = /^(?<method>GET|POST|PUT|PATCH) (?<path>\S+) HTTP\/(?<version>\d\.\d)\r\n(?<headers>.*\r\n)\r\n$/gms.exec(httpRequest)?.groups;
-    if (!regex) {
-      response(r404);
-      return;
-    }
-    const { method, path, version, headers } = regex;
-    for (let i = 0; i < acceptedPaths.length; i++) {
-      const acceptedPath: typeof acceptedPaths[number] = acceptedPaths[i];
-      const regexp = new RegExp(`^${acceptedPath}$`);
-      if (regexp.test(path)) {
-        if (acceptedPath.startsWith("/echo")) {
-          const endpoint = path.split("/")[2];
-          if (endpoint) {
-            response(r200, `Content-Type: text/plain\r\nContent-Length: ${endpoint.length}\r\n\r\n${endpoint}`);
-            return;
-          }
-        }
-        response(r200);
-        return;
-      }
-    }
-    response(r404);
-  });
-  socket.on("close", () => {
-    socket.end();
-  });
+	const response: Response = (res: string, body = ""): void => {
+		socket.write(res.concat(body, "\r\n"));
+		socket.end();
+	};
+	socket.on("data", (data) => {
+		const httpRequest = new TextDecoder().decode(data);
+		const regex =
+			/^(?<method>GET|POST|PUT|PATCH) (?<path>\S+) HTTP\/(?<httpVersion>\d\.\d)\r\n(?<headers>.*\r\n)\r\n$/gms.exec(
+				httpRequest,
+			)?.groups;
+		if (!regex) {
+			response(_404);
+			return;
+		}
+		const { method, path, httpVersion, headers } = regex;
+		const pathKey = Object.keys(requestHandler).find((handler) =>
+			new RegExp(`^${handler}$`).test(path),
+		);
+		if (!pathKey) {
+			response(_404);
+			return;
+		}
+		requestHandler[pathKey]({
+			method: method as Request["method"],
+			path,
+			httpVersion,
+			headers: Object.fromEntries(
+				headers.split("\r\n").map((header) => header.split(":")),
+			),
+			body: "",
+			response,
+		});
+		response(_404);
+	});
+	socket.on("close", () => {
+		socket.end();
+	});
 });
 
 server.listen(4221, "localhost");
